4696a6b03cdfee06d384cd35d00a15f7ffe7c681
Daniel Norberg <dano@spotify.com>
2015-02-01 12:35:49 +0100

    RadixTrie: real test

--- src/test/java/dano/TrieTest.java
+++ src/test/java/dano/TrieTest.java
@@ -2,6 +2,10 @@ package dano;
 
 import org.junit.Test;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,18 +13,29 @@ public class TrieTest {
 
   @Test
   public void testRealURIMap() {
-    final RadixTrie<String> radixTrie = RadixTrie.builder(String.class)
-        .insert("/usercount", "usercount")
-        .insert("/users", "users")
-        .insert("/users/<user>", "user")
-        .insert("/users/<user>/playlistcount", "user-playlistcount")
-        .insert("/users/<user>/playlists", "user-playlists")
-        .insert("/users/<user>/playlists/<playlist>/itemcount", "user-playlist-itemcount")
-        .insert("/users/<user>/playlists/<playlist>/items", "user-playlist-items")
-        .insert("/users/<user>/playlists/<playlist>/name", "user-playlist-name")
-        .insert("/users/<user>/playlists/<playlist>/items/<item>", "user-playlist-item")
-        .insert("/users/<user>/playlists/<playlist>", "user-playlist")
-        .build();
+    final Map<String, String> routes = new LinkedHashMap<String, String>();
+    routes.put("/usercount", "usercount");
+    routes.put("/users", "users");
+    routes.put("/users/<user>", "user");
+    routes.put("/users/<user>/playlistcount", "user-playlistcount");
+    routes.put("/users/<user>/playlists", "user-playlists");
+    routes.put("/users/<user>/playlists/<playlist>/itemcount", "user-playlist-itemcount");
+    routes.put("/users/<user>/playlists/<playlist>/items", "user-playlist-items");
+    routes.put("/users/<user>/playlists/<playlist>/name", "user-playlist-name");
+    routes.put("/users/<user>/playlists/<playlist>/items/<item>", "user-playlist-item");
+    routes.put("/users/<user>/playlists/<playlist>", "user-playlist");
+    final RadixTrie.Builder<String> builder = RadixTrie.builder(String.class);
+    for (final Map.Entry<String, String> entry : routes.entrySet()) {
+      builder.insert(entry.getKey(), entry.getValue());
+    }
+    final RadixTrie<String> rdx = builder.build();
+    for (final Map.Entry<String, String> entry : routes.entrySet()) {
+      if(!Objects.equals(rdx.lookup(entry.getKey()), entry.getValue())) {
+        final String value = rdx.lookup(entry.getKey());
+        System.out.println("mismatch: " + entry.getKey() + " -> " + value);
+      }
+      assertThat(rdx.lookup(entry.getKey()), is(entry.getValue()));
+    }
   }
 
   @Test
