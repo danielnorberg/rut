3306421fd11aaad182cb77cc7a8c6d54799c59fc
Daniel Norberg <dano@spotify.com>
2015-02-01 13:25:36 +0100

    Revert "cache length() within node scope"
    This reverts commit 0c6e494d9bd4ebfe02c2e442827fa73463ffe78c.

--- src/main/java/dano/RadixTrie.java
+++ src/main/java/dano/RadixTrie.java
@@ -67,13 +67,12 @@ public class RadixTrie<T> {
     }
 
     public T lookup(final CharSequence s, final int index) {
-      final int length = s.length();
-      final int next = match(s, index, length);
+      final int next = match(s, index);
       if (next == -1) {
         return null;
       }
       assert next > index;
-      if (next == length) {
+      if (next == s.length()) {
         return value;
       }
       final char c = s.charAt(next);
@@ -81,17 +80,17 @@ public class RadixTrie<T> {
       if (value != null) {
         return value;
       }
-      return capture(s, next, length);
+      return capture(s, next);
     }
 
-    private int match(final CharSequence s, final int index, final int length) {
-      if (index >= length) {
+    private int match(final CharSequence s, final int index) {
+      if (index >= s.length()) {
         return -1;
       }
       if (tail == null) {
         return index + 1;
       }
-      if (index + 1 + tail.length > length) {
+      if (index + 1 + tail.length > s.length()) {
         return -1;
       }
       for (int i = 0; i < tail.length; i++) {
@@ -102,11 +101,11 @@ public class RadixTrie<T> {
       return index + 1 + tail.length;
     }
 
-    private T capture(final CharSequence s, final int index, final int length) {
+    private T capture(final CharSequence s, final int index) {
       if (capture == null) {
         return null;
       }
-      final int maxCapture = seek(s, index, length, '/') - 1;
+      final int maxCapture = seek(s, index, '/') - 1;
       for (int i = maxCapture; i >= index; i--) {
         final char c = s.charAt(i);
         if (c == '/') {
@@ -151,9 +150,9 @@ public class RadixTrie<T> {
       return null;
     }
 
-    private int seek(final CharSequence s, final int start, final int length, final char c) {
+    private int seek(final CharSequence s, final int start, final char c) {
       int i = start;
-      for (; i < length; i++) {
+      for (; i < s.length(); i++) {
         if (s.charAt(i) == c) {
           return i;
         }
